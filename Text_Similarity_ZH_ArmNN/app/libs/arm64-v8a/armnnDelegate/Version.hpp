2d8a37359a95f5835daefa1caf9c2b537bfbe9c4
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-02-16 15:44:49 +0800

    Delete Text_Similarity_ZH_ArmNN directory

--- Text_Similarity_ZH_ArmNN/app/libs/arm64-v8a/armnnDelegate/Version.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Copyright © 2021-2023 Arm Ltd and Contributors. All rights reserved.
-// SPDX-License-Identifier: MIT
-//
-
-#include <armnn/delegate/classic/include/Version.hpp>
-#pragma message("armnn/delegate/include/Version.hpp has been deprecated, it is due for removal in"\
-                " 24.02 release. Please include armnn/delegate/classic/include/Version.hpp instead.")
