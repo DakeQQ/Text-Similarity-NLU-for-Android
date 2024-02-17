aadd7781a99d770cf4e9c6ee0544f6b077f49c71
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-02-17 13:06:05 +0800

    Update project.cpp

--- Text_Similarity_EN_ArmNN/app/src/main/cpp/project.cpp
+++ Text_Similarity_EN_ArmNN/app/src/main/cpp/project.cpp
@@ -2,7 +2,7 @@
 
 extern "C"
 JNIEXPORT jboolean JNICALL
-Java_com_example_myapplication_MainActivity_Load_1Models_10(JNIEnv *env, jclass clazz,
+Java_com_example_myapplication_MainActivity_Load_1Models_10(JNIEnv *env, jobject clazz,
                                                             jobject asset_manager,
                                                             jboolean use_fp16, jboolean use_bf16,
                                                             jboolean use_gpu, jboolean use_onnx) {
